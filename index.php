e914505a561542599892157d017502d97ee1cd62
Rhaissa <rhaisssa.msilva@gmail.com>
2024-11-13 21:38:06 -0300

    teste4

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         </div>
     </header>
     <div class="container content">
-        <h2>Bem-vindo à Toque de Cor – Onde Cada Cor Conta uma História!</h2>
+        <h2>Bem-vindo à Toque de Cor – Onde Ca</h2>
         <p>Na Toque de Cor, você encontra uma seleção incrível de esmaltes que refletem estilo e personalidade. De tons clássicos a cores ousadas, nossos produtos são escolhidos para transformar seu visual e destacar sua beleza. Além de uma variedade de esmaltes, oferecemos acessórios para garantir durabilidade e cuidado das suas unhas.</p>
         <p>Explore nosso site e descubra como um simples toque de cor pode elevar o seu estilo. Na Toque de Cor, cada detalhe importa, e estamos aqui para ajudar você a expressar sua verdadeira essência com confiança e charme!</p>
     </div>
